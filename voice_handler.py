8cda5716b3db756b6e73dd902011615f08018565
andrea <ferrandi.andrea@gmail.com>
2025-11-30 15:41:53 +0100

    use chromium, code refactored

--- voice_handler.py
+++ voice_handler.py
@@ -1,50 +1,19 @@
 
 # !/usr/bin/env python3
 
+import json
 import queue
-import subprocess
-from typing import Optional
-
-from playwright.sync_api import sync_playwright
+import re
+import sys
+from typing import Optional, Any
 
 import sounddevice as sd
+from _cffi_backend import buffer
+from playwright.sync_api import sync_playwright
 from vosk import Model, KaldiRecognizer
-import sys
-import json
-
-'''This script processes audio input from the microphone and displays the transcribed text.'''
-
-# list all audio devices known to your system
-print("Display input/output devices")
-devices = sd.query_devices()
-print(devices)
-
-sd.default.device= "Blue Snowball"
-# get the samplerate - this is needed by the Kaldi recognizer
-device_info = sd.query_devices(sd.default.device, 'input')
-samplerate = int(device_info['default_samplerate'])
-
-# display the default input device
-print("===> Initial Default Device Number:{} Description: {}".format(sd.default.device[0], device_info))
-
-# setup queue and callback function
-q = queue.Queue()
 
 
-def recordCallback(indata, frames, time, status):
-    if status:
-        print(status, file=sys.stderr)
-    q.put(bytes(indata))
 
-
-# build the model and recognizer objects.
-print("===> Build the model and recognizer objects.  This will take a few minutes.")
-MODEL_PATH = "vosk-model-small-en-us-0.15"
-model = Model(MODEL_PATH)
-recognizer = KaldiRecognizer(model, samplerate)
-recognizer.SetWords(False)
-
-print("===> Begin recording. Press Ctrl+C to stop the recording ")
 
 def open_browser(web_pages: list[str]):
     web_page_name = web_pages[0]
@@ -52,12 +21,15 @@ def open_browser(web_pages: list[str]):
     if web_page_url is not None:
         with sync_playwright() as p:
             print(f"open {web_page_url}")
-            browser = p.firefox.launch(headless=False)
+            browser = p.chromium.launch(headless=False)
             print(f"browser {browser}")
             page = browser.new_page()
             print(f"page {page}")
             page.goto(web_page_url)
-            page.wait_for_timeout(100000)
+            try:
+                page.wait_for_timeout(100000)
+            except:
+                print("Browser closed")
             #browser.close()
     else:
         print(f"web page not recognized: {web_page_name}")
@@ -73,11 +45,27 @@ def extract_web_page(web_page: str) -> Optional[str]:
             return None
 
 
-def in_netflix(action: list[str]):
-    pass
+def in_netflix(action: list[str]) -> None:
+    with sync_playwright() as p:
+        try:
+            # Connect to a running Chrome instance via CDP
+            browser = p.chromium.connect("http://localhost:9222")  # Replace 9222 with the actual debugging port
+            default_context = browser.contexts[0]
+            page = default_context.pages[0]  # Get the first page in the context
+            if "netflix" in page.url:
+                title = action[0]
+                regex = re.compile(f".*{title}.*", re.IGNORECASE)
+                anchor = page.get_by_role('link').get_by_label(regex)
+                print(f"Netflix click {anchor}")
+                anchor.click()
+                print(f"clicked on {title}")
+            else:
+                print(f"The page is not netflix but {page.title()}")
+        except Exception as e:
+            print(f"Executing netflix command {action} got {e}")
 
 
-def do_something(command_words: list[str]):
+def do_something(command_words: list[str]) -> None:
     if len(command_words) > 0:
         match command_words[0]:
             case "browser":
@@ -90,7 +78,7 @@ def do_something(command_words: list[str]):
         print("No command to run")
 
 
-def do_something_if_requested(words: list[str]):
+def do_something_if_requested(words: list[str]) -> None:
     words = [w for w in words if w not in   ["a", "and", "but"]]
     if len(words) > 0:
         first_word = words[0]
@@ -99,27 +87,69 @@ def do_something_if_requested(words: list[str]):
         elif first_word == "hi" and len(words) >= 3:
             do_something_if_requested(words[1:])
         else:
-            print("Not a command")
+            print(f"Not a command: {words}")
     else:
-        print("Nothing to do")
+        print("No command, nothing to do")
 
 
-try:
-    with sd.RawInputStream(dtype='int16', callback=recordCallback):
-        while True:
-            data = q.get()
-            if recognizer.AcceptWaveform(data):
-                result_text = recognizer.Result()
-                # convert the recognizerResult string into a dictionary
-                result_dict = json.loads(result_text)
-                text = result_dict.get("text", "")
-                if text != "":
-                    print(f"Text from microphone: {text}")
-                    do_something_if_requested(text.split(" "))
-                else:
-                    print("no input sound")
+def read_from_microphone(recognizer: KaldiRecognizer, data: Any) -> None:
+    if recognizer.AcceptWaveform(data):
+        result_text = recognizer.Result()
+        # convert the recognizerResult string into a dictionary
+        result_dict = json.loads(result_text)
+        text = result_dict.get("text", "")
+        if text != "":
+            print(f"Text from microphone: {text}")
+            do_something_if_requested(text.split(" "))
+        else:
+            print("no input sound")
 
-except KeyboardInterrupt:
-    print('===> Finished Recording')
-except Exception as e:
-    print(str(e))
+def init_recognizer() -> KaldiRecognizer:
+    # list all audio devices known to your system
+    print("Display input/output devices")
+    devices = sd.query_devices()
+    print(devices)
+    sd.default.device = "Blue Snowball"
+    # get the samplerate - this is needed by the Kaldi recognizer
+    device_info = sd.query_devices(sd.default.device, 'input')
+    samplerate = int(device_info['default_samplerate'])
+    # build the model and recognizer objects.
+    print("===> Build the model and recognizer objects.  This will take a few minutes.")
+    MODEL_PATH = "vosk-model-small-en-us-0.15"
+    model = Model(MODEL_PATH)
+    recognizer = KaldiRecognizer(model, samplerate)
+    recognizer.SetWords(False)
+    # display the default input device
+    print(f"===> Initial Default Device Number:{sd.default.device[0]} Description: {device_info}")
+    return recognizer
+
+class RecordCallbackHandler:
+    def __init__(self, q: queue.Queue[Any]):
+        self._q = q
+    def recordCallback(self, indata: buffer, frames: int, time: None, status: sd.CallbackFlags) -> None:
+        if status:
+            print(status, file=sys.stderr)
+        self._q.put(bytes(indata))
+
+def recording_loop(q: queue.Queue[Any], recognizer: KaldiRecognizer):
+    print("===> Begin recording. Press Ctrl+C to stop the recording ")
+    callback_handler = RecordCallbackHandler(q)
+    try:
+        with sd.RawInputStream(dtype='int16', callback=callback_handler.recordCallback):
+            while True:
+                data = q.get()
+                read_from_microphone(recognizer, data)
+    except KeyboardInterrupt:
+        print('===> Finished Recording')
+    except Exception as e:
+        print(str(e))
+
+def main():
+    '''This script processes audio input from the microphone and displays the transcribed text.'''
+    recognizer = init_recognizer()
+    # setup queue and callback function
+    q = queue.Queue()
+    recording_loop(q, recognizer)
+
+if __name__ == "__main__":
+    main()
