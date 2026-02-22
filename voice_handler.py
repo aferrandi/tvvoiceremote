23b8d3ad382ef0eff9620d6f0ad00543960306bf
andrea <ferrandi.andrea@gmail.com>
2026-02-22 22:36:39 +0100

    fix main method

--- voice_handler.py
+++ voice_handler.py
@@ -2,14 +2,11 @@
 # !/usr/bin/env python3
 
 import queue
-import sys
-from dataclasses import dataclass
 
-from config_reader import Config, read_config
+from config_reader import read_config
 from microphone.microphone_init import init_recognizer
 from microphone.microphone_loop import recording_loop
 
-@dataclass(frozen=True)
 
 def main():
     config = read_config()
