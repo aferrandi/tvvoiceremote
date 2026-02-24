145eec7c71e47c9fad9d0ce2a74805e83b95be9d
andrea <ferrandi.andrea@gmail.com>
2026-02-24 22:01:58 +0100

    sounds are mp3

--- utils/sounds.py
+++ utils/sounds.py
@@ -2,10 +2,10 @@ from playsound import playsound
 
 
 def play_correct() -> None:
-    playsound(r'sounds/correct.wav')
+    playsound(r'sounds/correct.mp3')
 
 def play_error() -> None:
-    playsound(r'sounds/error.wav')
+    playsound(r'sounds/error.mp3')
 
 def print_error(text: str) -> None:
     print(text)
