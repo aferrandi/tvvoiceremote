254664fe71aa0c235f75e8c63f176b1cede68351
andrea <ferrandi.andrea@gmail.com>
2026-02-24 22:08:50 +0100

    try with preferredsoundplayer

--- utils/sounds.py
+++ utils/sounds.py
@@ -1,11 +1,11 @@
-from playsound import playsound
+from preferredsoundplayer import soundplay
 
 
 def play_correct() -> None:
-    playsound(r'sounds/correct.mp3')
+    soundplay(r'sounds/correct.mp3')
 
 def play_error() -> None:
-    playsound(r'sounds/error.mp3')
+    soundplay(r'sounds/error.mp3')
 
 def print_error(text: str) -> None:
     print(text)
